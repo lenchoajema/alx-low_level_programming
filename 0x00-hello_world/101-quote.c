0c81f50302fb46327437c60e97be11f25c9c4abb
lenchoajema <lenchoajema@gmail.com>
2023-02-18 00:07:03 +0300

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - starting point
+ * Return: return 1 on success
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful \" - Dora Korpar, 2015-10-19");
+	return (1);
+}
