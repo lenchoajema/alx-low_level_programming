4f48998fb2d7ede126eaf755607f96f150f677aa
lenchoajema <lenchoajema@gmail.com>
2023-02-17 23:35:38 +0300

    C with betty introduction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - Entry point
  * Return: Always 0 (Success)
@@ -12,7 +12,7 @@ int main(void)
 	float f:
 
 	printf("Size of a char: %lu byte(s) \n", (unsigned long)sizeof(d));
-	printf("Size of a int: %lu byte(s) \n", (unsigned long)sizeof(a));
+	printf("Size of an int: %lu byte(s) \n", (unsigned long)sizeof(a));
 	printf("Size of a long int: %lu byte(s) \n", (unsigned long)sizeof(b));
 	printf("Size of a long long int: %lu byte(s) \n", (unsigned long)sizeof(c));
 	printf("Size of a float: %lu byte(s) \n", (unsigned long)sizeof(f));
