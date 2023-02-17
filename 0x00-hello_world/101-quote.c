a47d28fa9df0aa2d1c579a9d02b4c3df65eb2040
lenchoajema <lenchoajema@gmail.com>
2023-02-18 00:13:50 +0300

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * main - starting point
  * Return: return 1 on success
  */
 int main(void)
 {
-	fwrite(STDOUT_FILENO, "and that piece of art is useful \" - Dora Korpar, 2015-10-19");
+	fwrite(STDOUT_FILENO, "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
