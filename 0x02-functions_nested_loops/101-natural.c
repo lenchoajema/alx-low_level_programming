bfe94194e60af32ded1feab573404a98e172df06
Lenchoajema <lenchoajema@gmail.com>
2023-02-22 00:39:54 -0800

    sum of natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: return 1 on success
+ */
+int main(void)
+{
+int sum = 0;
+for (int i = 0; i < 1024; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+sum += i;
+}
+printf(" the sum of all the multiples of 3 or 5 below 1024: %d\n", sum);
+return (0);
+}
