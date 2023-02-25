5ff93cea812a4821eda2ffc1e256f6fdde7000fb
Lenchoajema <lenchoajema@gmail.com>
2023-02-24 17:00:10 -0800

    multiple question answer

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,16 +7,16 @@
 void main(void)
 {
 int i;
- for (i = 1;i <= 100; i++)
-   {
-     if ( i % 15 == 0)
-       printf("FizzBuzz ");
-     else if (i % 5 == 0)
-       printf("Buzz ");
-     else if (i % 3 == 0)
-       printf("Fizz ");
-     else
-       printf("%d ", i);
-   }
- printf("\n");
+for (i = 1;i <= 100; i++)
+{
+if ( i % 15 == 0)
+printf("FizzBuzz ");
+else if (i % 5 == 0)
+printf("Buzz ");
+else if (i % 3 == 0)
+printf("Fizz ");
+else
+printf("%d ", i);
+}
+printf("\n");
 }
