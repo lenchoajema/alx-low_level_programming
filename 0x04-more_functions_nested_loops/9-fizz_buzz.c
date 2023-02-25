2fe54285667542017987ec71a13bb8c4ff2cbf97
Lenchoajema <lenchoajema@gmail.com>
2023-02-24 17:12:25 -0800

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,9 +7,9 @@
 int main(void)
 {
 int i;
-for (i = 1;i <= 100; i++)
+for (i = 1; i <= 100; i++)
 {
-if ( i % 15 == 0)
+if (i % 15 == 0)
 printf("FizzBuzz ");
 else if (i % 5 == 0)
 printf("Buzz ");
