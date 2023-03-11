3eca67250257f36e149123ccc477939b1ed3009b
Lenchoajema <lenchoajema@gmail.com>
2023-03-11 12:45:35 -0800

    multiplication

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,12 +11,18 @@ int main(int argc, char *argv[])
 int num1, num2, mul = 0;
 if (argc != 3)
 {
+if (argc == 1)
+	printf("%d\n", argc - 1);
+else
 	printf("Error\n");
-	return (1);
+return (1);
 }
+else
+{
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
-mul = num1 * num2;
+mul = num1 *num2;
 printf("%d\n", mul);
+}
 return (0);
 }
