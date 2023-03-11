95ce0fb2bd7e9bd970aaa95a38c4a61dbe79475d
Lenchoajema <lenchoajema@gmail.com>
2023-03-11 12:19:55 -0800

    Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+int num1, num2, mul = 0;
+if (argc != 3)
+{
+	printf("Error\n");
+	return (1);
+}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+mul = num1 * num2;
+printf("%d\n", mul);
+return (0);
+}
