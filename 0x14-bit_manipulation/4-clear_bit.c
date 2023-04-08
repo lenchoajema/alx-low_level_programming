24fe68ceb1e7db0ac5ffb227118051cb453a9080
Lenchoajema <lenchoajema@gmail.com>
2023-04-07 19:57:16 -0700

    bit manipulation 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * clear_bit - to clear the value
+ * @n: to store the the bit data
+ * @index: it shows the position ot place
+ * Return: 1 on successful else -1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{       
+        if (!n || index >= sizeof(*n) * 8)
+                return (-1);
+        *n &=0 << index;
+        return (1);
+}       
