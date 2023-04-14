868774fd5e52f613af6da9c328b2877bae400c31
Lenchoajema <lenchoajema@gmail.com>
2023-04-14 15:06:10 -0700

    Create a function that creates a file.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,38 +11,38 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer = NULL;
-	ssize_t b_read;
-	ssize_t b_written;
-	int fd;
+	char *buffer1 = NULL;
+	ssize_t b_read1;
+	ssize_t b_written1;
+	int fd1;
 
 	if (!(filename && letters))
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	fd1 = open(filename, O_RDONLY);
+	if (fd1 == -1)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (!buffer)
+	buffer1 = malloc(sizeof(char) * letters);
+	if (!buffer1)
 		return (0);
 
-	b_read = read(fd, buffer, letters);
-	close(fd);
+	b_read1 = read(fd1, buffer1, letters);
+	close(fd1);
 
-	if (b_read < 0)
+	if (b_read1 < 0)
 	{
-		free(buffer);
+		free(buffer1);
 		return (0);
 	}
-	if (!b_read)
-		b_read = letters;
+	if (!b_read1)
+		b_read1 = letters;
 
-	b_written = write(STDOUT_FILENO, buffer, b_read);
-	free(buffer);
+	b_written1 = write(STDOUT_FILENO, buffer1, b_read1);
+	free(buffer1);
 
-	if (b_written < 0)
+	if (b_written1 < 0)
 		return (0);
 
-	return (b_written);
+	return (b_written1);
 }
