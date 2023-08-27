3f3c76d2cca367163ba25fe0bf3673633b2396f3
Lencho Ajema <lenchoajema@gmail.com>
2023-08-27 12:24:44 -0700

    advanced for 0x18

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-./gm 9 8 10 24 75 9
+wget -p /tmp https://github.com/lenchoajema/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD=/tmp/libgiga.so
+
