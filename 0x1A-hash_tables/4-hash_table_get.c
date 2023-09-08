fce9682f03faa241734a21ff74f8a36ef418fdb0
Lencho Ajema <lenchoajema@gmail.com>
2023-09-08 14:27:12 -0700

    hash table

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,10 +12,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
     unsigned long int len;
     hash_node_t *tmp;
 
-    if (ht == NULL || ht->array == NULL || ht->len == 0 ||
+    if (ht == NULL || ht->array == NULL || ht->size == 0 ||
         key == NULL || strlen(key) == 0)
         return (NULL);
-    len = key_index((const unsigned char *)key, ht->len);
+    len = key_index((const unsigned char *)key, ht->size);
     tmp = ht->array[len];
     while (tmp != NULL)
     {
